412ee53ca8e3fbe85e5cf65a287484b1bffd3b6a
BastianThoma <ossi_1993@hotmail.de>
2025-07-22 09:28:08 +0200

    Hide login logo on small screens when scrolled
    Added logic to hide the blue login logo when the user scrolls on small screens (<390px width). Updated styles and markup to support the new hidden state and added a divider to the login card for improved UI separation.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -10,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   logoAnimActive = false;
+  hideLogo: boolean = false;
 
   ngOnInit() {
     setTimeout(() => {
       this.logoAnimActive = true;
     }, 1000); // Startet die Animation nach 1 Sekunde
+
+    window.addEventListener('scroll', this.handleScrollOrResize);
+    window.addEventListener('resize', this.handleScrollOrResize);
+    this.handleScrollOrResize();
   }
-}
+
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.handleScrollOrResize);
+    window.removeEventListener('resize', this.handleScrollOrResize);
+  }
+
+  handleScrollOrResize = () => {
+    const isSmallScreen = window.innerWidth < 390;
+    const isScrolled = window.scrollY > 0;
+    this.hideLogo = isSmallScreen && isScrolled;
+  }
+  }
+
