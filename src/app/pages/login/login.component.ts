9d58d9d481073a53b3a2347656ea7722cc2bbcc8
BastianThoma <ossi_1993@hotmail.de>
2025-07-23 09:52:52 +0200

    Add password visibility toggle to login form
    Introduced a show/hide password feature in the login form by adding visibility toggle buttons and corresponding icons. Updated the component to manage password visibility state and bound the password input to a model. Adjusted styles to support the new UI elements.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,16 +1,21 @@
 
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+
   logoAnimActive = false;
   hideLogo: boolean = false;
+
+  password: string = '';
+  showPassword: boolean = false;
 
   ngOnInit() {
     setTimeout(() => {
@@ -32,5 +37,8 @@ export class LoginComponent implements OnInit {
     const isScrolled = window.scrollY > 0;
     this.hideLogo = isSmallScreen && isScrolled;
   }
-  }
 
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+}
